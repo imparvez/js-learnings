cb59501dd85998883dcf0126f75cc6e1e6703117
Parvez Shaikh <parvezshaikhj@gmail.com>
2016-12-12 00:06:47 +0530

    Array toString method

---
+++ arrays/array-toString.js
@@ -0,0 +1,8 @@
+var month = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
+
+//Print the array
+console.log(month);
+
+
+//Print the string containing all the elements in array
+console.log(month.toString());
